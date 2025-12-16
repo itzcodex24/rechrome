81e49fa23ea2491a99c60c305b05e060424899e0
Andrei <andrei.cherciu24@gmail.com>
2025-12-16 12:40:48 +0000

    Closes seo

---
+++ app/sitemap.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = process.env.NEXT_PUBLIC_URL || 'https://rechrome.dev';
+
+  return [
+    {
+      url: baseUrl,
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+  ];
+}
