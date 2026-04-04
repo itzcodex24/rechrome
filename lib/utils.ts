273a146343b710efe1be5d020db955672cd552ba
Andrei <andrei.cherciu24@gmail.com>
2026-04-04 16:29:18 +0100

    fix(#7): add missing mac-x64 versions

--- lib/utils.ts
+++ lib/utils.ts
@@ -5,7 +5,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
-export const PrimaryPlatforms = ['android', 'mac', 'linux', 'linux64', 'win', 'win64'] as const
+export const PrimaryPlatforms = ['android', 'mac-arm64', 'mac-x64', 'linux64', 'win', 'win64'] as const
 export const SecondaryPlatforms = ['linux64', 'mac-arm64', 'mac-x64', 'win32', 'win64'] as const
 
 export type TPrimaryPlatforms = typeof PrimaryPlatforms[number]
